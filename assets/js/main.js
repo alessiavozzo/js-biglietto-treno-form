8439204f49ca10d783659a56a0556f0ab8b269fd
alessiavozzo <a.vozzo95@gmail.com>
2024-02-20 17:23:51 +0100

    add console log printing ticket price

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,27 @@
 // Variables connected to html
-const userKm = document.getElementById("user-km");  
+const userKm = document.getElementById("user-km");
 const userAge = document.getElementById("user-age");
 const userBtn = document.getElementById("user-btn");
 
-console.log(userKm.value);
-
 // Price variables
 const costPerKm = 0.21;
-const fullPrice = costPerKm * userKm.value;
 const minorDiscount = 0.2;
 const seniorDiscount = 0.4;
 
-/* let ticketPrice;
+userBtn.addEventListener('click', function () {
 
-if(userAge.value < 18){
-    ticketPrice = (fullPrice - (fullPrice * minorDiscount)).toFixed(2);
-}
-else if(userAge.value > 65){
-    ticketPrice = (fullPrice - (fullPrice * seniorDiscount)).toFixed(2);
-}
-else{
-    ticketPrice = fullPrice.toFixed(2);
-}
+    const fullPrice = costPerKm * userKm.value;
+    let ticketPrice;
 
- */
+    if (userAge.value < 18) {
+        ticketPrice = (fullPrice - (fullPrice * minorDiscount)).toFixed(2);
+    }
+    else if (userAge.value > 65) {
+        ticketPrice = (fullPrice - (fullPrice * seniorDiscount)).toFixed(2);
+    }
+    else {
+        ticketPrice = fullPrice.toFixed(2);
+    }
 
-// Click-btn
-/* userBtn.addEventListener('click', function(){
-    alert(ticketPrice)
-}) */
+    console.log(ticketPrice);
+})
