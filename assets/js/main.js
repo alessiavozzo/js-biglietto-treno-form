6eb3e1b681df91c4c3ee28cbf73bca25574ba89d
alessiavozzo <a.vozzo95@gmail.com>
2024-02-20 18:16:07 +0100

    add print ticket

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,16 @@
 // Variables connected to html
+//user
 const userKm = document.getElementById("user-km");
 const userAge = document.getElementById("user-age");
 const userBtn = document.getElementById("user-btn");
+const userName = document.getElementById("user-name");
+
+//ticket
+const ticketName = document.querySelector(".ticket-name");
+const ticketOffer = document.querySelector(".ticket-offer");
+const ticketPrintPrice = document.querySelector(".ticket-print-price");
+const ticketCode = document.querySelector(".random-code");
+const ticketWagon = document.querySelector(".wagon");
 
 // Price variables
 const costPerKm = 0.21;
@@ -11,17 +20,28 @@ const seniorDiscount = 0.4;
 userBtn.addEventListener('click', function () {
 
     const fullPrice = costPerKm * userKm.value;
+    
     let ticketPrice;
+
 
     if (userAge.value < 18) {
         ticketPrice = (fullPrice - (fullPrice * minorDiscount)).toFixed(2);
+        ticketOffer.innerHTML = "Minor ticket";
     }
     else if (userAge.value > 65) {
         ticketPrice = (fullPrice - (fullPrice * seniorDiscount)).toFixed(2);
+        ticketOffer.innerHTML = "Senior ticket";
     }
     else {
         ticketPrice = fullPrice.toFixed(2);
+        ticketOffer.innerHTML = "Adult ticket";
     }
 
-    console.log(ticketPrice);
+    // console.log(ticketPrice);
+
+    ticketName.innerHTML = userName.value; 
+    ticketCode.innerHTML = Math.ceil(Math.random() * 100000);
+    ticketWagon.innerHTML = Math.ceil(Math.random() * 10);
+    ticketPrintPrice.innerHTML = `${ticketPrice} €`;
+
 })
